8ac7f6346efd9858900ad522e3a1ebcfbadf884e
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-09-16 20:17:20 -0300

    feat: add tests

--- railstestes/app/models/user.rb
+++ railstestes/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+
+  def titulo_completo
+    "#{self.name} Email: #{self.email}"
+  end
 end
--- railstestes/spec/models/user_spec.rb
+++ railstestes/spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validade de um Usuário" do
+    it "Usuário válido" do
+      user = User.new
+      user.name = "John"
+      user.email = "john@john.com"
+
+      expect(user.valid?).to be_truthy 
+    end
+
+    it "Usuário inválido" do
+      user = User.new
+      user.name = "John"
+      user.email = nil
+
+      expect(user.valid?).to be_falsey
+    end
+  end
+
+  context "Títulos válidos" do
+    it "Título completo" do
+      user = User.new
+      user.name = "John"
+      user.email = "john@john.com"
+
+      expect(user.titulo_completo).to eql("Sr. John - Email: john@john.com") 
+    end
+  end
+  
 end
