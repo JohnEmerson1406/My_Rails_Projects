48186319c37680334c3d32e69bdf6557c1eec726
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-10-22 16:18:14 -0300

    feat: add new solution for i18n date

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,11 +1,17 @@
 class Contact < ApplicationRecord
   belongs_to :kind #, optional: true
 
-  def to_br
-    {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    }
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
   end
 end
