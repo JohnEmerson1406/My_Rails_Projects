fc997d8a0ce74d13de6514d00e91433ed914ed29
John Emerson <john.carv.sousa@gmail.com>
2021-01-24 20:20:21 -0300

    feat: add layout for login

--- csvapp/app/controllers/application_controller.rb
+++ csvapp/app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "session"
+    else
+      "application"
+    end
+  end
 
   protected
 
